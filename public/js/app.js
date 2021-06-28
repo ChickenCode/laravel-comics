3980f3626fc43c99ed51f776885eae56c3b47a9a
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-06-28 09:44:54 -0400

    styling main

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\caval\Desktop\boolean\Laravel_projects\laravel-comics\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\caval\Desktop\boolean\Laravel_projects\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\caval\desktop\boolean\laravel_projects\laravel-comics\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\caval\desktop\boolean\laravel_projects\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -19,27 +19,28 @@
         </div>
     
 </section>
-
-<section style="background-color: #0282f9;">
-    <div>
+<div style="background-color: #0282f9;">
+<section  class="flex lower-sect container" style="justify-content: space-between">
+    <div class="flex align-items-center">
         <img src="{{'images/buy-comics-digital-comics.png'}}" alt="">
         <p>DIGITAL COMICS</p>
     </div>
-    <div>
+    <div class="flex align-items-center">
         <img src="{{'images/buy-comics-merchandise.png'}}" alt="">
         <p>DC MERCHANDISE</p>
     </div>
-    <div>
+    <div class="flex align-items-center">
         <img src="{{'images/buy-comics-subscriptions.png'}}" alt="">
         <p>SUBSCRIPTION</p>
     </div>
-    <div>
+    <div class="flex align-items-center">
         <img src="{{'images/buy-comics-shop-locator.png'}}" alt="">
         <p>COMIC SHOP LOCATOR</p>
     </div>
-    <div>
+    <div class="flex align-items-center">
         <img src="{{'images/buy-dc-power-visa.svg'}}" style="width: 100px" alt="">
         <p>DC POWER VISA</p>
     </div>
 </section>
+</div>
 @endsection
