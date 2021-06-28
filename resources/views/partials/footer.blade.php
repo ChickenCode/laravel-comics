75cdd39c074b2b6f9abfa4f204da7d9bff8c4feb
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-06-28 10:13:17 -0400

    main task completa, manca bonus

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,69 +1,77 @@
 
-    <section>
-        <div>
+    <section class="top-footer">
+        <div class="container flex">
             <div>
                 <h3>DC COMICS</h3>
                 <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
                 </ul>
-                <h3>SHOP</h3>
+                <h3 style="margin-top: 10px">SHOP</h3>
                 <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
                 </ul>
             </div>
 
             <div>
                 <h3>DC</h3>
                     <ul>
-                        <li>Terms Of Use</li>
-                        <li>Privacy Policy</li>
-                        <li>Ad Choiches</li>
-                        <li>Advertising</li>
-                        <li>Jobs</li>
-                        <li>Subscriptions</li>
-                        <li>Talent Workshops</li>
-                        <li>CPSC Certificates</li>
-                        <li>Ratings</li>
-                        <li>Shop Help</li>
-                        <li>Contact Us</li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Ad Choiches</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
                     </ul>
             </div>
             
             <div>
                 <h3>SITES</h3>
                     <ul>
-                        <li>DC</li>
-                        <li>MAD Magazine</li>
-                        <li>DC Kids</li>
-                        <li>DC Universe</li>
-                        <li>DC Powes Visa</li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Powes Visa</a></li>
                     </ul>
             </div>
         </div>
         
-        <div>
-            <p>All Sites Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="">noted here. all rights reserved</a></p>
-            <p><a href="">Cookies Settings</a></p>
+        <div class="container">
+            <p style="color: gray; font-size:0.8em;">All Sites Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="" style="color: #0282f9">noted here</a>. all rights reserved</p>
+            <p><a href="" style="color: #0282f9">Cookies Settings</a></p>
         </div>
     </section>
-        <div>
 
-    <section>
-        </div>
-            <a href="">SIGN-UP NOW!</a>
-        <div>
-            <h3>FOLLOW US</h3>
+
+
+
+
+
+<section class="black-ground " style="padding-top: 20px">
+    <div class="container flex align-items-center" style="justify-content: space-between">
+    <div class="sign-up">
+                <a href="" style="color: white">SIGN-UP NOW!</a>
+    </div>
+    <div class="flex" style="transform: translateY(5px)">
+            <h3 style="color: #0282f9; margin-right: 8px;">FOLLOW US</h3>
             <img src="{{asset('images/footer-facebook.png')}}" alt="">
             <img src="{{asset('images/footer-twitter.png')}}" alt="">
             <img src="{{asset('images/footer-youtube.png')}}" alt="">
             <img src="{{asset('images/footer-pinterest.png')}}" alt="">
             <img src="{{asset('images/footer-periscope.png')}}" alt="">
         </div>
+
+    </div>
     </section>
